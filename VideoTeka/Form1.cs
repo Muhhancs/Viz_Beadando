647bea07193cdb9b1152aab58c7c6c5354a7a638
Gal Szabolcs <131382486+galszabiii@users.noreply.github.com>
2023-04-26 16:31:49 +0200

    VideoTeka

---
+++ VideoTeka/Form1.cs
@@ -0,0 +1,91 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using System.Data.SqlClient;
+
+namespace VideoTeka
+{
+    public partial class Form1 : Form
+    {
+        List<loginData> userLoginData;
+        
+        public Form1()
+        {
+            getDataFromDatabase();
+            InitializeComponent();
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            string user = userText.Text;
+            string pwd = pwdText.Text;
+
+            string pwdFromDatabase = getPwd(user);
+            if (pwdFromDatabase != "error") chechkIfPwdCorrect(pwd, pwdFromDatabase);
+            else {
+                errorMsg.Text = "Incorrect login data!";
+                errorMsg.Visible = true;
+                userText.Clear();
+                pwdText.Clear();
+            }
+        }
+
+        private void chechkIfPwdCorrect(string pwd, string inDataBase)
+        {
+            if(pwd==inDataBase)
+            {
+                errorMsg.Visible=true;
+                errorMsg.Text = "Correct login data!";
+                //opennewTab
+            }
+            else
+            {
+                errorMsg.Text = "Incorrect login data!";
+                errorMsg.Visible = true;
+                userText.Clear();
+                pwdText.Clear();
+            }
+        }
+
+        private string getPwd(string user)
+        {
+            string result = "error";
+            for (int i = 0; i < userLoginData.Count(); i++)
+            {
+                if(user == userLoginData[i].Username || user == userLoginData[i].Email) result = userLoginData[i].Password;
+            }
+            return result;
+        }
+
+        private void getDataFromDatabase()
+        {
+            userLoginData = new List<loginData>();
+
+            const string connectionString = "server=mysql.nethely.hu;port=3306;database=galack;uid=galack;password=Webprog2023;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            string sqlCmd = "SELECT * FROM VT_user ";
+            MySqlCommand command = new MySqlCommand(sqlCmd, connection);
+
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string uName = reader.GetString(1);
+                string email = reader.GetString(2);
+                string pwd = reader.GetString(3);
+
+                loginData a = new loginData(id, uName, email, pwd);
+                userLoginData.Add(a);
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ VideoTeka/loginData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoTeka
+{
+    internal class loginData
+    {
+        int id;
+        string username;
+        string email;
+        string password;
+
+        public loginData(int a, string b, string c, string d) { 
+            this.id = a;
+            this.username = b;
+            this.email = c;
+            this.password = d;
+        }
+
+        public string Password {
+            get { return password; }
+        }
+        public string Username
+        {
+            get { return username; }
+        }
+        public string Email
+        {
+            get { return email; }
+        }
+
+    }
+}
